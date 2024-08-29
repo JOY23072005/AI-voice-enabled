c13046d01364ba42d0634d9875c5c79a125006ff
Joydeep Hans <144369315+JOY23072005@users.noreply.github.com>
2024-08-30 01:52:42 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -17,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    app = get_wsgi_application()
 
 if __name__ == '__main__':
     main()
