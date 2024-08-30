57750ce07b4fe5747093f1c616faad84b162106a
Joydeep Hans <144369315+JOY23072005@users.noreply.github.com>
2024-08-30 09:56:53 +0530

    Update settings.py

--- VoiceEnabledAI/settings.py
+++ VoiceEnabledAI/settings.py
@@ -17,6 +17,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://ai-voice-enabled-mo4ukp2fg-joy23072005s-projects.vercel.app/history',
+]
 
 # Application definition
 
